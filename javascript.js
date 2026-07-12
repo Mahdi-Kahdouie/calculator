ae039a3a6c59eb978457a37da8679b81a6c8fe51
Mahdi Kahdouie <Mahdi.Kahdouie@gmail.com>
2026-07-12 21:08:52 +0330

    Add all features that explain in exercise

--- javascript.js
+++ javascript.js
@@ -1,23 +1,49 @@
 let input="";
+let i=0;
+let resultDone=0;
 
 
 
 
 
 
+const calculatorButton = document.querySelector("#calculatorButton");
+const display = document.querySelector(".calculatorDisplay");
 
-const calculatorBody = document.querySelector("#calculatorBody");
-
-calculatorBody.addEventListener('click',(event) => {
+calculatorButton.addEventListener('click',(event) => {
 
     if(event.target.classList.contains('button')){
         if((event.target.textContent)=="=")
             operate(input);
-        else if((event.target.textContent)=="C")
+        else if((event.target.textContent)=="C"){
             input=""
+            display.textContent = 0
+            resultDone = 0
+        }
         else{
-        input+=(event.target.textContent);
-        console.log(input)
+
+        let operators=["+", "-", "*", "/"]
+        let clickButton=event.target.textContent
+        let lastChar=input.slice(-1)
+
+        if(resultDone==1 && (!(operators.includes(clickButton)))){
+            input=""
+            resultDone=0 
+        }
+        else if (resultDone == 1 && operators.includes(clickButton)) {
+                resultDone = 0; 
+        }
+
+        if(operators.includes(clickButton) && operators.includes(lastChar))
+            input = input.slice(0, -1);
+
+        else if (operators.includes(clickButton)) {
+            if (input.includes("+") || input.includes("-") || input.includes("*") || input.includes("/")) 
+                operate(input);
+        }
+
+        input += clickButton;
+        display.textContent = input;
         }
 
     }
@@ -46,7 +72,6 @@ function operate(str){
         calcute(array,"/")
         }
     
-    console.log(array)
 
 
 }
@@ -67,10 +92,29 @@ function calcute(array,operator){
         result=Number(array[0])*Number(array[1]);
 
     if(operator=="/")
-        result=Number(array[0])/Number(array[1]);
+        if(Number(array[1])==0)
+            result = "Error"
+        else
+            result=Number(array[0])/Number(array[1]);
 
     
-    console.log(result)
+    if(result=="Error"){
+        display.textContent="Error"
+        input=""
+
+    }
+    else if (Number.isInteger(result)) {
+    display.textContent = result; 
+    input=String(result)
+    resultDone=1
+    } else {
+    display.textContent = result.toFixed(3); 
+    result=result.toFixed(3);
+    input=String(result)
+    resultDone=1
+    }
+
+    
 
     
 }
